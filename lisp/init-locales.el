14cbd77cd55d28704804f200755deb0166b614f0
Kevin li <kevinli@Kevins-MacBook-Air.local>
2021-01-31 15:23:00 +0800

    disable m-x for Counsel-M-x

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -109,7 +109,7 @@
 (setq ivy-use-virtual-buffers t)
 (setq ivy-count-format "(%d/%d) ")
 (global-set-key (kbd "C-s") 'swiper-isearch)
-(global-set-key (kbd "M-x") 'counsel-M-x)
+;;(global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "M-y") 'counsel-yank-pop)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
