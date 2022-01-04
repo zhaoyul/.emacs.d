5152e9f6d11c69dd07786612e8d9d7a75dc6d082
kevin li <kli@kli.com>
2022-01-04 19:12:42 +0800

    goon

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -96,6 +96,9 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((R . t)
+   (awk . t)
+   (dot . t)
+   (plantuml . t)
    (clojure . t)
    (latex . t)
    (python . t)
@@ -108,6 +111,7 @@
    (emacs-lisp . t)
    (lisp . t)    ;; slime - lisp interaction mode
    (gnuplot . t)))
+(setq org-plantuml-jar-path "~/.emacs.d/plantuml.jar")
 
 (setq org-confirm-babel-evaluate t)
 
@@ -220,7 +224,54 @@
 (venv-initialize-eshell)
 
 (setq org-babel-clojure-backend 'cider)
+
 
+;; Region lines and then `M-x osx-say' to make OSX speak.
+
+;; Adjust speak speed
+(setq osx-say-speed 180)
+
+;; Change voice
+;; Kathy, Vicki, Victoria, Alex, Bruce, Fred
+(setq osx-say-voice "Alex")
+(setq osx-say-buffer "*osx say*")
+
+(defun osx-say-stop ()
+  (interactive)
+  (when (get-buffer osx-say-buffer)
+    (kill-buffer osx-say-buffer)))
+
+(defun osx-say (&optional $word $speed)
+  "Utilize `say' command that Mac OSX has."
+  (interactive)
+  (unless (executable-find "say")
+    (error (message "`say' command not found")))
+  (osx-say-stop)
+  (cond ($word $word)
+        (mark-active
+         (setq $word (buffer-substring-no-properties
+                      (region-beginning) (region-end))))
+        ((setq $word (thing-at-point 'word)))
+        (t (setq $word (read-string "word: "))))
+  (mapc (lambda ($r)
+          (setq $word (replace-regexp-in-string (car $r) (cdr $r) $word)))
+        (list ;;'("'"   . "\\\\'")
+         '("\""  . "\\\\\"")
+         '("?"   . "\\\\?")
+         '("\n"  . " ")
+         '("\("  . "\\\\(")
+         '("\)"  . "\\\\)")
+         '("\\[" . "\\\\[")
+         '("\\]" . "\\\\]")
+         '("\;"  . "\\\\;")
+         '("\&"  . "\\\\&")
+         '("\|"  . "\\\\|")))
+  (save-window-excursion
+    (start-process "OSX Say" osx-say-buffer
+                   "say" "-v" osx-say-voice "-r"
+                   (number-to-string (or $speed osx-say-speed)) $word)))
+
+
 
 (provide 'init-locales)
 ;;; init-locales.el ends here
